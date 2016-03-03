11b5dfe954469d7685142ba585feccd027b7c9b5
YoshGuenther <guenther.joshua@yahoo.com>
2016-03-03 14:34:49 -0600

    Added Masonry.js for grid alignment. Currently it is not working.

--- js/loadImages.js
+++ js/loadImages.js
@@ -1,19 +1,42 @@
-images = ["AllState","AmericanCampus","AustinDentalWorks","BarberShop","BBVCompass","BestLittlePawnShop","Binger","Ferrari",
-"EcoClean","Bucks","CafeMenet","CutiePis","ElectricAvenue","EnhancedViewingExperience","EVOEntertainment","FerrariHorse","BrakeCheck",
-"FerrariOFAustin","Flowerland","FrozenCave","GalaxyCafe","LaCabana","PieSociety","SaltGrass","SetonMarbleFalls","Tacos","TheGallery",
-"WeBuyGold"];
+images = ["AllState","AmericanCampus","AustinDentalWorks","BarberShop","BBVCompass","BestLittlePawnShop","Binger","BrakeCheck","Bucks","CafeMenet","CutiePis","EcoClean","ElectricAvenue","EnhancedViewingExperience","EVOEntertainment","Ferrari","FerrariHorse","FerrariOFAustin","Flowerland","FrozenCave","GalaxyCafe","InStep","JerseyMike'sSubs","LaCabana","MamaFu'sAsianHouse","MaozVegetarian","MattressFirm","PieSociety","SaltGrass","SetonMarbleFalls","Tacos","TheGallery","WeBuyGold"
+];
+
+//http://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+function shuffle(array){
+  var currentIndex = array.length, temporaryValue, randomIndex;
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+  return array;
+}
+
 function loadImage(imageNames, size, divSelector){
   imageNames.forEach(function(entry){
     img = new Image();
     img.src = "images/"+size+"/"+entry+".png";
-    img.className = "images"
+    img.width = "286"
+    img.className = "grid-item"
     divSelector.appendChild(img);
-    console.log(entry);
   });
 }
 
 window.onload = function () {
+  images = shuffle(images)
+  var elem = document.querySelector('.grid');
+ loadImage(images, "small", elem);
 
- loadImage(images, "small", document.getElementById("backgroundImages"));
+
+ console.log(elem);
+ var msnry = new Masonry( elem, {
+    itemSelector: '.grid-item',
+    columnWidth: 286,
+  });
 
 }
