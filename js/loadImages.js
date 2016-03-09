22d651a41aa7bd62704f1996f1f31d4e5b223037
YoshGuenther <guenther.joshua@yahoo.com>
2016-03-08 20:22:52 -0600

    Added search functionality that repopulates page. The actual search is not working though, but the page gets repopulated.

--- js/loadImages.js
+++ js/loadImages.js
@@ -1,6 +1,46 @@
 images = ["7Eleven","AllState","AmericanCampus","AustinDentalWorks","Avella","BarberShop","BBVCompass","BestLittlePawnShop","Bfree","Binger","BrakeCheck","Bucks","BurgerTexII","CafeExpress","CafeMenet","CentralChiropractic","Changos","ChangosMonkey","ChangosTaqueria","CutiePis","DiabloRojoTatooCross","DiatoJapaneseRestraunt","EcoClean","ElectricAvenue","EnhancedViewingExperience","EVOEntertainment","EyeJoy","Ferrari","FerrariHorse","FerrariOFAustin","Flowerland","FrozenCave","GalaxyCafe","GreenGos","GuzuGallery","HappyHour","HeartHospitalOfAustin","HoundsToothCoffe","IHeartVideo","IndependentBank","InStep","JerseyMike'sSubs","Julians","LaCabana","MamaFu'sAsianHouse","MaozVegetarian","MattressFirm","Maudie'sTex-Mex","medspringurgentcare","MossyGeneral&CosmeticDentistry","P","PieSociety","PTerry's","RedStella","Reid's","SaltGrass","SetonMarbleFalls","SmoothieKing","SnapKitchen","Tacodeli","Tacos","Tatoo","TheGallery","WeBuyGold","WEST","YogurtPlanet"
 ];
 
+function setup(imageArray){
+  loadImage(imageArray);
+  console.log("loaded images:")
+  initializeGrid(imageArray);
+}
+
+window.onload = function(){
+  listOfImages = images
+  listOfImages = shuffle(listOfImages)
+  setup(listOfImages);
+  var button = document.getElementById("searchButton");
+  button.onlick = searchAndFilter();
+}
+
+
+
+
+  function initializeGrid(imageArray){
+    //http://stackoverflow.com/questions/5706757/is-there-a-way-to-check-document-ready-if-jquery-is-not-available
+    console.log("initializing grid");
+    var interval = 100;
+    var deltaTime = 0;
+    var elem = document.querySelector('#grid');
+    var tid = setInterval(function (){
+      if(document.readyState !== 'complete'){
+        return;
+      }
+      else if(loadedImages == imageArray.length || deltaTime >= 500){
+        clearInterval(tid);
+        var msnry = new Masonry(elem, {
+          itemSelector: '.grid-item',
+          singleMode: true,
+        });
+      }
+      deltaTime += interval;
+    }, interval );
+  }
+
+
+
 //http://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
 function shuffle(array){
   var currentIndex = array.length, temporaryValue, randomIndex;
@@ -17,64 +57,44 @@ function shuffle(array){
   return array;
 }
 
-function loadImage(imageNames, size, divSelector){
-  divSelector.innerHTML = "";
+var loadedImages;
+function loadImage(imageNames){
   loadedImages = 0;
-  imageNames.forEach(function(entry){
+  console.log("in load image")
+  console.log(imageNames)
+  var divSelector = document.getElementById("grid");
+  divSelector.innerHTML = "";
+  for(var i = 0; i < imageNames.length; i++){
     var div = document.createElement('div');
     div.className = "grid-item";
 
     img = new Image();
-    var url = "images/"+size+"/"+entry+".png";
+    var url = "./images/small/"+imageNames[i]+".png";
     img.src = url;
     img.width = 286;
     img.onload = function(){loadedImages++};
 
     div.appendChild(img);
     divSelector.appendChild(div);
-  });
+  }
 }
 
 console.log("about to onload")
 
-window.onload = function(){
-  listOfImages = images
-  listOfImages = shuffle(listOfImages)
-  var elem = document.querySelector('#grid');
-  loadImage(listOfImages, "small", elem);
-  console.log("loaded images:")
-
-  //http://stackoverflow.com/questions/5706757/is-there-a-way-to-check-document-ready-if-jquery-is-not-available
-  var interval = 100;
-  var deltaTime = 0;
-  var tid = setInterval(function (){
-    if(document.readyState !== 'complete'){
-      return;
-    }
-    else if(loadedImages == images.length || deltaTime >= 50000){
-      clearInterval(tid);
-      var msnry = new Masonry(elem, {
-        itemSelector: '.grid-item',
-        singleMode: true,
-      });
-    }
-    deltaTime += interval;
-  }, interval );
-
-  var button = document.getElementById("searchButton");
-  button.onlick = searchAndFilter();
-}
 
 var searchAndFilter = function(){
+  console.log("called")
   var searchBox = document.getElementById("search")
   var searchTerm = searchBox.value;
-  console.log(searchTerm);
   if(searchTerm != undefined){
-    var dupArray = images.slice();
-    for(var i = 0; i < dupArray.length; i++){
-      if(dupArray[i].indexOf(searchTerm) < 0){ //Doesn't contain term
+    var duplicateArray = images.slice();
+    for(var i = 0; i < duplicateArray.length; i++){
+      if(duplicateArray[i].indexOf(searchTerm) < 0){ //Doesn't contain term
+        duplicateArray.splice(i, 1)
       }
     }
+  console.log(duplicateArray)
+  setup(duplicateArray);
   }
 }
 
